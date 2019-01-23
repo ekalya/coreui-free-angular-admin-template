0d12df8bf81b58b0b2e7efaf6e464c4b647586ba
Elisha Kalya <ekalya@localhost.localdomain>
2019-01-22 20:09:32 -0700

    Added api service

---
+++ .history/src/app/services/api.service_20190122200746.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+  private formatErrors(error: any) {
+    return  throwError(error.error);
+  }
+
+  get<T>(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http.get<T>(`${environment.api_url}${path}`, { params })
+      .pipe(catchError(this.formatErrors));
+  }
+
+  put<T>(path: string, body: Object = {}): Observable<any> {
+    return this.http.put(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body)
+    ).pipe(catchError(this.formatErrors));
+  }
+
+  post<T>(path: string, body: Object = {}): Observable<any> {
+    return this.http.post(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body)
+    ).pipe(catchError(this.formatErrors));
+  }
+
+  delete<T>(path): Observable<any> {
+    return this.http.delete(
+      `${environment.api_url}${path}`
+    ).pipe(catchError(this.formatErrors));
+  }
+}
--- .history/src/app/services/company.service_20190122195353.ts
+++ .history/src/app/services/company.service_20190122195353.ts
@@ -1,28 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Company } from '../models/company';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
-  resource = '/company';
+  resource = 'company';
   constructor(private apiService: ApiService) { }
 
   create(company: Company): Observable<Company> {
-    return this.apiService.put<Company>(this.resource, company);
+    return this.http.put<Company>(this.baseUrl, company);
   }
   getAll(): Observable<Company[]> {
-    return this.apiService.get<Company[]>(this.resource);
+    return this.http.get<Company[]>(this.baseUrl);
   }
   getById(id: number): Observable<Company> {
-    return this.apiService.get<Company>(this.resource + id);
+    return this.http.get<Company>(this.baseUrl + id);
   }
   update(company: Company): Observable<Company> {
-    return this.apiService.post<Company>(this.resource, company);
+    return this.http.post<Company>(this.baseUrl, company);
   }
   delete(id: number): Observable<Company> {
-    return this.apiService.delete<Company>(this.resource + id);
+    return this.http.delete<Company>(this.baseUrl + id);
   }
 }
